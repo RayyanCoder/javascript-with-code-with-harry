3a7051e2dc4eeb8afff2f76da2312c6a98290102
rayyanlearning <rayyanlearning2022@gmail.com>
2022-10-20 05:13:48 -0700

    Day2-completed
    operators,conditional statments ,loops
    Date:-20/10/2022

---
+++ Day2/operator.js
@@ -0,0 +1,47 @@
+let a=50;
+let b = 10;
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a**b);
+console.log(a%b);
+//first print a then increament
+console.log("a++",a++);
+//first print a then increament
+
+console.log("++a",++a);
+
+// lecture number 6 basic operators see from google
+
+// let age = prompt("enter the age ");
+// age  = Number.parseInt(age);
+// if(age>0){
+//     alert("age is valid");
+// }
+
+//above code is valid but not run due running environment is node js
+//conditional statment and if else and switch and ternary operators are ther you can watch  
+// lecture 8 includes practice set on if and else statements
+
+//lecture 9 loops
+ for(let i=1;i<=10;i++){
+    console.log(i);
+ }
+
+ const bike ={
+    name:"honda cbr2",
+    price:3000,
+    color:'red'
+ }
+
+ for(let b in bike){
+    console.log(bike[b]);
+ }
+
+ let bikeName = "honda shine";
+ for (let bname of bikeName){
+    console.log(bname);
+ }
+
+ //lecture 10 video contains only while lopp you can go through from mdn

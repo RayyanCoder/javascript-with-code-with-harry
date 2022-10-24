448eb3b3dec98aee625d6a94f7f293b247b2c088
rayyanlearning <rayyanlearning2022@gmail.com>
2022-10-24 13:45:28 +0530

    Day5-completed
    all basics of js

---
+++ Day5/array_practice.js
@@ -0,0 +1,5 @@
+// lecture not 21 is simple practice of array normal programs is there
+//lecture no 22 is also basic program
+//lecture no 23  is also general knowledge program
+// lecture no 24 is also basic program
+//lecture no 25 is also general program
